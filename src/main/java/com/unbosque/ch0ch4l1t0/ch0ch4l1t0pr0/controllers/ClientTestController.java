fbe6a479a44c8cc2ea8f43ea4adf139639b4ae50
DevJohann <felipepoveda273@gmail.com>
2024-05-22 22:24:00 -0500

    Cliente 30% listo

--- src/main/java/com/unbosque/ch0ch4l1t0/ch0ch4l1t0pr0/controllers/ClientTestController.java
+++ src/main/java/com/unbosque/ch0ch4l1t0/ch0ch4l1t0pr0/controllers/ClientTestController.java
@@ -1,14 +1,22 @@
 package com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.entities.Reserva;
+import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.MesaService;
+import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.ReservaService;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.SedeService;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.TipoReservaService;
 
@@ -24,21 +32,33 @@ public class ClientTestController {
     @Autowired
     private TipoReservaService tipoReservaService;
 
+    //Servicio de las reservas
+    @Autowired
+    private ReservaService reservaService;
+
+    //Servicio de las mesas
+    @Autowired
+    private MesaService mesaService;
+
 
     //Métodos para retornar las plantillas Thymeleaf
     @GetMapping("/reservaCliente")
     public String crearReserva(Model model){
-        System.out.println("Cargando sedes...");
+        //System.out.println("Cargando sedes...");
         //Genera la lista de sedes usando el servicio
         model.addAttribute("sedes", sedeService.listarSedes());
 
-        System.out.println("Cargando tipos de reserva...");
+        //System.out.println("Cargando tipos de reserva...");
         //Genera la lista de tipos de reserva usando el servicio
         model.addAttribute("tiposReserva", tipoReservaService.listarTodos());
 
         //Creación del objeto que se va a popular en el formulario (Llega luego al POST)
         model.addAttribute("reserva", new Reserva());
-        System.out.println("Carga exitosa, recuperando reservaCliente");
+
+        //Toca pasarle las mesas disponibles en la mesa
+        //model.addAttribute("mesasSede")
+
+        //System.out.println("Carga exitosa, recuperando reservaCliente");
         return "reservaCliente";
     }
 
@@ -46,6 +66,19 @@ public class ClientTestController {
     @PostMapping("/reservaCliente")
     public String verReserva(@ModelAttribute("reserva") Reserva reserva, Model model){
         //Verificar que la reserva esté disponible
-        return "redirect:/reservaCliente";
+        //Reserva reserva = reservaService.
+
+        System.out.println("Operación exitosa");
+        return "redirect:/testcliente/reservaCliente";
+    }
+
+    //----------------------FIN HACER RESERVA
+
+    //Método para el formato de la fecha
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
     }
 }
