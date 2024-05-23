5a85c7ba63209f2d92c464bf2c45077676107b1f
DevJohann <felipepoveda273@gmail.com>
2024-05-22 23:53:23 -0500

    Update

--- src/main/java/com/unbosque/ch0ch4l1t0/ch0ch4l1t0pr0/controllers/ClientTestController.java
+++ src/main/java/com/unbosque/ch0ch4l1t0/ch0ch4l1t0pr0/controllers/ClientTestController.java
@@ -3,6 +3,7 @@ package com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.controllers;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.entities.Mesa;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.entities.Reserva;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.MesaService;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.ReservaService;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.SedeService;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.TipoReservaService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/testcliente")
@@ -45,7 +49,7 @@ public class ClientTestController {
 
     //Métodos para retornar las plantillas Thymeleaf
     @GetMapping("/reservaCliente")
-    public String crearReserva(Model model){
+    public String crearReserva(Model model, HttpServletRequest request){
         //System.out.println("Cargando sedes...");
         //Genera la lista de sedes usando el servicio
         model.addAttribute("sedes", sedeService.listarSedes());
@@ -54,23 +58,40 @@ public class ClientTestController {
         //Genera la lista de tipos de reserva usando el servicio
         model.addAttribute("tiposReserva", tipoReservaService.listarTodos());
 
+        
+        //Toca pasarle las mesas disponibles en la mesa
+        model.addAttribute("mesas", request.getAttribute("mesas"));
+        System.out.println("Cargando mesas de la sede...");
+
         //Creación del objeto que se va a popular en el formulario (Llega luego al POST)
         model.addAttribute("reserva", new Reserva());
 
-        //Toca pasarle las mesas disponibles en la mesa
-        //model.addAttribute("mesasSede")
-
-        //System.out.println("Carga exitosa, recuperando reservaCliente");
+        System.out.println("Carga exitosa, recuperando reservaCliente");
         return "reservaCliente";
     }
 
     //Este método recibe el formulario de crear una reserva
+    //Método que se usa para guardar la reserva en la DB
     @PostMapping("/reservaCliente")
     public String verReserva(@ModelAttribute("reserva") Reserva reserva, Model model){
         //Verificar que la reserva esté disponible
         //Reserva reserva = reservaService.
 
         System.out.println("Operación exitosa");
+        return "redirect:/testcliente/reservaCliente";
+    }
+
+    //Método para buscar las mesas disponibles antes de reservar
+    @PostMapping("/cargarMesa")
+    public String verificarMesa(@ModelAttribute("reserva") Reserva reserva, HttpServletRequest request, Model model){
+        
+        Long idSede = reserva.getFkSede();
+        System.out.println("Buscando sede " + idSede);
+        java.util.List<Mesa> mesasDeSede = mesaService.mesasPorSedeId(idSede);
+        System.out.println("Encontradas " + mesasDeSede.size() + " mesas");
+        //No envía las mesas encontradas
+        request.setAttribute("mesas", mesasDeSede);
+        
         return "redirect:/testcliente/reservaCliente";
     }
 
