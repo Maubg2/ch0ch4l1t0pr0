781e34f8a8422caae3d119e9234e1ccf30a97716
Maubg2 <mauriciobel2002@gmail.com>
2024-05-23 12:18:29 -0500

    Sedes Listo

--- src/main/java/com/unbosque/ch0ch4l1t0/ch0ch4l1t0pr0/controllers/ControllerTestEmpleado.java
+++ src/main/java/com/unbosque/ch0ch4l1t0/ch0ch4l1t0pr0/controllers/ControllerTestEmpleado.java
@@ -17,6 +17,7 @@ import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.repositories.SedeRepository;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.AuditoriaService;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.MesaService;
 import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.SedeService;
+import com.unbosque.ch0ch4l1t0.ch0ch4l1t0pr0.services.TipoReservaService;
 
 @Controller
 public class ControllerTestEmpleado {
@@ -30,12 +31,15 @@ public class ControllerTestEmpleado {
     @Autowired
     private MesaService mesaService;
 
+    @Autowired
+    private TipoReservaService tipoReservaService;
+
     @GetMapping("/menuAdmin")
     public String test(){
         return "menuAdmin";
     }
 //////////////////////////////////////////////////////////
-
+  
     @GetMapping("/sedesAdmin")
     public String sedesAdmin(Model model) {
         List<Sede> sedes = sedeService.listarSedes();
@@ -75,13 +79,20 @@ public class ControllerTestEmpleado {
     @GetMapping("/verMesas/{idSede}")
     public String verMesas(@PathVariable Long idSede, Model model) {
         List<Mesa> mesas = mesaService.mesasPorSedeId(idSede);
-        System.out.println("////////////////////////////////////////////////////////////////////////////////////");
-        for(Mesa x : mesas){
-            System.out.println(x.getId());
-            System.out.println(x.getCantidadPersonas());
-        }
         model.addAttribute("mesas", mesas);
+        model.addAttribute("idSede", idSede);
         return "listadoMesasAdmin";
+    }
+
+    @GetMapping("/modificarMesa/{idMesa}")
+    public String mostrarFormularioModificarMesa(@PathVariable Long idMesa, Model model) {
+        Optional<Mesa> mesa = mesaService.findById(idMesa);
+        if (mesa.isPresent()) {
+            model.addAttribute("mesa", mesa.get());
+        } else {
+            model.addAttribute("mesa", new Mesa());
+        }
+        return "modificarMesaAdmin";
     }
 
     @PostMapping("/modificarMesa/{idMesa}")
@@ -90,58 +101,13 @@ public class ControllerTestEmpleado {
         return "redirect:/verMesas/" + mesaModificada.getFkSede();
     }
 
-/*  
-@GetMapping("/sedesAdmin")
-public String sedesAdmin(Model model) {
-    List<Sede> sedes = sedeService.listarSedes();
-    model.addAttribute("sedes", sedes);
-    return "sedesAdmin";
-}
-
-@GetMapping("/modificarSedeAdmin/{idSede}")
-public String mostrarFormularioModificarSede(@PathVariable Long idSede, Model model) {
-    Optional<Sede> sede = sedeService.findById(idSede);
-    if (sede.isPresent()) {
-        model.addAttribute("sede", sede.get());
-    } else {
-        model.addAttribute("sede", new Sede());
-    }
-    return "modificarSedeAdmin";
-}
-
-@PostMapping("/modificarSedeAdmin/{idSede}")
-public String modificarSedeAdmin(@PathVariable Long idSede, @ModelAttribute Sede sedeModificada) {
-    sedeService.modificarSede(idSede, sedeModificada);
-    return "redirect:/sedesAdmin";
-}
-
-@GetMapping("/crearSede")
-public String mostrarFormularioCrearSede(Model model) {
-    model.addAttribute("sede", new Sede());
-    return "modificarSedeAdmin";
-}
-
-@PostMapping("/crearSede")
-public String crearSede(@ModelAttribute Sede nuevaSede) {
-    sedeService.guardar(nuevaSede);
-    return "redirect:/sedesAdmin";
-}
-
-@GetMapping("/verMesas/{idSede}")
-public String verMesas(@PathVariable Long idSede, Model model) {
-    List<Mesa> mesas = mesaService.mesasPorSedeId(idSede);
-    model.addAttribute("mesas", mesas);
-    return "listadoMesasAdmin";
-}
-
-
-    }*/
-
 //////////////////////////////////////////////////////////
     @GetMapping("/reservasAdmin")
-    public String reservasAdmin(){
+    public String reservasAdmin(Model model){
+        model.addAttribute("tipoReservas", tipoReservaService.listarTodos());
         return "reservasAdmin";
     }
+
 //////////////////////////////////////////////////////////
     @GetMapping("/verClientesAdmin")
     public String verClientesAdmin(){
